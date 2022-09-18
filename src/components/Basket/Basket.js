fc89f127ddcba4c9b98b670079e228b482474d67
Darío Fernández <dariofedes@protonmail.com>
2022-09-18 13:03:51 +0200

    feat: add basket product

--- src/components/Basket/Basket.js
+++ src/components/Basket/Basket.js
@@ -1,12 +1,35 @@
 import './styles.sass'
+import PropTypes from 'prop-types'
+import { productType } from '../LineProduct/LineProduct'
 
-export default function Basket() {
+export default function Basket({ products = [] }) {
+    const formatPrice = (price) => {
+        const euroFormatter = Intl.NumberFormat("es-ES", {
+            style: "currency",
+            currency: "EUR",
+        });
+
+        return euroFormatter.format(price)
+    }
+
+    const basketProduct = (product) => (
+        <li className='basket__product basket-product' key={product.id}>
+            <p className='basket-product__name'>{product.name}</p>
+            <p className='basket-product__price'>{formatPrice(product.price)}</p>
+        </li>
+    )
+
     return (
         <div className='basket'>
             <div className='basket__header'>
             <h1 className='basket__title'>MI CESTA:</h1>
             </div>
             <div className='basket__product-list' />
+            <ul className='basket__product-list'>
+                {
+                    products.map(product => basketProduct(product))
+                }
+            </ul>
             <div className='basket__footer'>
             <div className='basket__total-products'>
                 <p className='basket__total-title'>TOTAL</p>
@@ -16,4 +39,12 @@ export default function Basket() {
             </div>
         </div>
     )
+}
+
+Basket.propTypes = {
+    products: PropTypes.arrayOf(productType)
+}
+
+Basket.defaultProps = {
+    products: []
 }
--- src/components/Basket/Basket.spec.js
+++ src/components/Basket/Basket.spec.js
@@ -18,4 +18,41 @@ describe('Basket', () => {
         expect(totalProducts).toBeInTheDocument()
         expect(totalPrice).toBeInTheDocument()
     })
+
+    it('should show products added to the basket', () => {
+        // Given
+        const products = [
+            {
+                id: 1,
+                name: 'A product name 1',
+                price: 100,
+                image: 'an-image-uri.jpg'
+            },
+            {
+                id: 2,
+                name: 'A product name 2',
+                price: 200,
+                image: 'an-image-uri.jpg'
+            },
+            {
+                id: 3,
+                name: 'A product name 3',
+                price: 300,
+                image: 'an-image-uri.jpg'
+            },
+        ]
+
+        // When
+        render(<Basket products={products} />)
+
+        // Then
+        const productsInBasket = screen.getAllByRole('listitem')
+        expect(productsInBasket).toHaveLength(3)
+        expect(screen.getByText('A product name 1')).toBeInTheDocument()
+        expect(screen.getByText('100,00 €')).toBeInTheDocument()
+        expect(screen.getByText('A product name 2')).toBeInTheDocument()
+        expect(screen.getByText('200,00 €')).toBeInTheDocument()
+        expect(screen.getByText('A product name 3')).toBeInTheDocument()
+        expect(screen.getByText('300,00 €')).toBeInTheDocument()
+    })
 })
