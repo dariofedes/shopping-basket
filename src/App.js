a696a4ac0b38902a0ec31a82e16270b8dd4e7dbd
Darío Fernández <dariofedes@protonmail.com>
2022-09-20 15:40:41 +0200

    feat: add error boundary

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
+import { ErrorBoundary } from 'react-error-boundary'
 import ProductList from './components/LineProductList/LineProductList'
 import Basket from './components/Basket/Basket'
 import './App.sass'
+import ErrorFallback from './components/ErrorFallback/ErrorFallback'
 
 function App() {
   return (
     <div className='app'>
-      <ProductList className='app__product-list' />
-      <Basket className='app__basket' />
+      <ErrorBoundary FallbackComponent={ErrorFallback}>
+        <ProductList className='app__product-list' />
+        <Basket className='app__basket' />
+      </ErrorBoundary>
     </div>
   )
 }
---
+++ src/components/ErrorFallback/ErrorFallback.js
@@ -0,0 +1,8 @@
+export default function ErrorFallback() {
+  return (
+    <div>
+      <h1>Ups...</h1>
+      <p>Ha ocurrido un error. Por favor, recargue la página.</p>
+    </div>
+  )
+}
